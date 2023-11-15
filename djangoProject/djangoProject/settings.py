ba980214b6379f8a677e9534a2894248c627674f
noname_d <davletbaev-timur@mail.ru>
2023-11-15 17:31:33 +0300

    add registration.beta1 noname_td

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from django.urls import reverse_lazy
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -127,3 +129,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = reverse_lazy("main:myforms")
--- djangoProject/main/views.py
+++ djangoProject/main/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 def main(request):
     return render(request, 'main/main.html')
 
 
+@login_required
 def myforms(request):
     # проверка авторизован или нет
     # если нет то идет на auth
