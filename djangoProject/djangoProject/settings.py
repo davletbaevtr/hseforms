5eb7106477790739a1fd384a1f63b2a18afb43f1
noname_d <davletbaev-timur@mail.ru>
2023-11-16 01:31:55 +0300

    fix setting.py noname_td

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -16,8 +16,6 @@ from django.urls import reverse_lazy
 
 from decouple import config
 
-config = config()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
