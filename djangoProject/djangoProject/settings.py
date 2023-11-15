56aa19c0f1e77ed1efd58814e3892d57b0e9e1cc
noname_d <davletbaev-timur@mail.ru>
2023-11-15 23:18:27 +0300

    add Argon2 password hasher and update requirements.txt noname_td

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -103,6 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
