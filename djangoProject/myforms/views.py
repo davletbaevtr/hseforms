90c48f85139652d11c5a68d918ddc7c3cb2eaefe
noname_d <davletbaev-timur@mail.ru>
2023-11-17 14:02:43 +0300

    update edit logic noname_td

--- djangoProject/myforms/views.py
+++ djangoProject/myforms/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 from djangoProject.myforms.models import Survey
@@ -15,5 +15,14 @@ def create(request):
     return redirect('edit_survey', unique_id=survey.unique_id)
 
 
-def edit(request):
-    return render(request, 'myforms/myforms.html')
+def edit(request, unique_id):
+    survey = get_object_or_404(Survey, unique_id=unique_id)
+
+    # Проверка, является ли пользователь создателем опроса
+    if request.user != survey.creator:
+        # Возврат ошибки или перенаправление, если пользователь не создатель
+        return redirect('access_error_page')
+
+    # Логика редактирования опроса
+
+    return render(request, 'myforms/edit_survey.html', {'survey': survey})
