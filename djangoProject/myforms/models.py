9cf78b5fbc47100727db339ccc7dfbf0d99a7285
noname_d <davletbaev-timur@mail.ru>
2023-11-17 13:46:05 +0300

    update models.py for myforms noname_td

--- djangoProject/myforms/models.py
+++ djangoProject/myforms/models.py
@@ -1,23 +1,31 @@
+import uuid
 from django.db import models
 
+
 class Survey(models.Model):
-    title = models.CharField(max_length=255) # название опроса
-    description = models.TextField() # описание опроса
+    unique_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)  # уникальный id опроса
+
+    title = models.CharField(max_length=255)  # название опроса
+    description = models.TextField()  # описание опроса
 
     def __str__(self):
         return self.title
-class Option(models.Model):
-    choice = models.CharField(max_length=100) # название для нового варианта
-    is_answer = models.BooleanField(default=False) # является ли данный вариант ответом
 
-    def __str__(self):
-        return self.choice
+
 class Question(models.Model):
     survey = models.ForeignKey(Survey, on_delete=models.CASCADE, related_name='questions')
-    number = 0 # какой вопрос по счету
-    question = models.TextField() # название вопроса
-    is_checkbox = False # TextField or options Field
-    options = models.ManyToManyField(Option, related_name='checkbox_questions') # варианты ответов
+    number = 0  # какой вопрос по счету
+    question = models.TextField()  # название вопроса
+    is_checkbox = False  # TextField or options Field
 
     def __str__(self):
         return f'{self.survey.title} - Question {self.number}: {self.question}'
+
+
+class Option(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='question')
+    choice = models.CharField(max_length=100)  # название для нового варианта
+    is_answer = models.BooleanField(default=False)  # является ли данный вариант ответом
+
+    def __str__(self):
+        return self.choice
